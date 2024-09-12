7f3cab26067bc80f8678e23b14bad6b171da2af2
Matenza <164885821+Matenza@users.noreply.github.com>
2024-09-12 15:47:05 +0000

    Update vocl.py

--- vocl.py
+++ vocl.py
@@ -42,6 +42,79 @@ def download_transcription():
 def stop_listening():
 	st.session_state['run'] = False
 
+
+# Send audio (Input) / Receive transcription (Output)
+async def send_receive():
+	URL = f"wss://api.assemblyai.com/v2/realtime/ws?sample_rate={RATE}"
+
+	print(f'Connecting websocket to url ${URL}')
+
+	async with websockets.connect(
+		URL,
+		extra_headers=(("Authorization", st.secrets['api_key']),),
+		ping_interval=5,
+		ping_timeout=20
+	) as _ws:
+
+		r = await asyncio.sleep(0.1)
+		print("Receiving messages ...")
+
+		session_begins = await _ws.recv()
+		print(session_begins)
+		print("Sending messages ...")
+
+
+		async def send():
+			while st.session_state['run']:
+				try:
+					data = stream.read(FRAMES_PER_BUFFER)
+					data = base64.b64encode(data).decode("utf-8")
+					json_data = json.dumps({"audio_data":str(data)})
+					r = await _ws.send(json_data)
+
+				except websockets.exceptions.ConnectionClosedError as e:
+					print(e)
+					assert e.code == 4008
+					break
+
+				except Exception as e:
+					print(e)
+					assert False, "Not a websocket 4008 error"
+
+				r = await asyncio.sleep(0.01)
+
+
+		async def receive():
+			while st.session_state['run']:
+				try:
+					result_str = await _ws.recv()
+					result = json.loads(result_str)['text']
+
+					if json.loads(result_str)['message_type']=='FinalTranscript':
+						print(result)
+						st.session_state['text'] = result
+						st.write(st.session_state['text'])
+
+						transcription_txt = open('transcription.txt', 'a')
+						transcription_txt.write(st.session_state['text'])
+						transcription_txt.write(' ')
+						transcription_txt.close()
+
+
+				except websockets.exceptions.ConnectionClosedError as e:
+					print(e)
+					assert e.code == 4008
+					break
+
+				except Exception as e:
+					print(e)
+					assert False, "Not a websocket 4008 error"
+			
+		send_result, receive_result = await asyncio.gather(send(), receive())
+
+
+asyncio.run(send_receive())
+
 def is_microphone_available():
     try:
         p = pyaudio.PyAudio()
